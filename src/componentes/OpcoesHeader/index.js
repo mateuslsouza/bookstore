3ec86a5a2af057062402d2311432b6f323a454bd
mateus lima <mlimadesouza14@gmail.com>
2025-11-13 15:24:30 -0300

    refactor(styles): remove component-specific CSS files and update Header structure
    - Deleted old style files:
      - src/App.css
      - src/componentes/IconesHeader/style.css
      - src/componentes/Logo/style.css
      - src/componentes/OpcoesHeader/style.css
    - Updated components to use new styling approach:
      - Header, IconesHeader, Logo, OpcoesHeader
    - Updated App.js and index.js to reflect the new structure

--- src/componentes/OpcoesHeader/index.js
+++ src/componentes/OpcoesHeader/index.js
@@ -1,16 +1,32 @@
-import "./style.css";
+import styled from "styled-components";
 
-const textoOpcoes = ["CATEGORIAS", "FAVORITOS", "MINHA ESTANTE"];
+const Opcao = styled.li`
+  font-size: 16px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  height: 100%;
+  padding: 0 5px;
+  cursor: pointer;
+  min-width: 120px;
+`;
+
+const Opcoes = styled.ul`
+  display: flex;
+`;
+
+const textoOpcoes = ["CATEGORIES", "FAVORITES", "MY BOOKSHELF"];
 
 function OpcoesHeader() {
   return (
-    <ul className="opcoes">
+    <Opcoes>
       {textoOpcoes.map((texto) => (
-        <li className="opcao">
+        <Opcao className="opcao">
           <p>{texto}</p>
-        </li>
+        </Opcao>
       ))}
-    </ul>
+    </Opcoes>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createGlobalStyle } from "styled-components";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const GlobalStyle = createGlobalStyle`
+body{
+  margin: 0;
+}
+li{
+  list-style:none;
+}
+`;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>
 );
